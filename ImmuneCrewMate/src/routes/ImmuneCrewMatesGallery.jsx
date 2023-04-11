fa7a3d68a6e1940aef0eb383b44511d1f42063c8
Arnold Bhebhe <legendaryarnoldb@gmail.com>
2023-04-10 22:44:54 -0500

    update readme + add walkthrough

--- ImmuneCrewMate/src/routes/ImmuneCrewMatesGallery.jsx
+++ ImmuneCrewMate/src/routes/ImmuneCrewMatesGallery.jsx
@@ -14,6 +14,7 @@ import { Outlet } from "react-router-dom";
 import AntigenSpecificityChart from "../visualizations/AntigenSpecificityChart";
 import CellTypeChart from "../visualizations/CellTypeChart";
 import EffectorFunctionsChart from "../visualizations/EffectorFunctionsChart";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const ImmuneCrewMatesGallery = () => {
   const theme = useTheme();
@@ -27,134 +28,150 @@ const ImmuneCrewMatesGallery = () => {
 
   return (
     <>
-      <Typography
-        variant="h3"
-        style={{
-          textAlign: "center",
-        }}
-      >
-        Your ImmuneCrewMates Gallery
-        <Typography
-          style={{
-            color: theme.palette.text.secondary,
-            textAlign: "center",
-            padding: "1rem",
-          }}
-        >
-          Explore your ImmuneCrewMates and learn more about them. Check our your
-          ImmuneCrewMates' stats and see how they compare to other
-          ImmuneCrewMates.
-        </Typography>
-      </Typography>
-
-      <Grid container spacing={1}>
-        <Grid item xs={8}>
-          <ImageList
-            sx={{
-              width: "65vw",
-              height: "80vh",
+      {immuneCrewMates.length > 0 ? (
+        <>
+          <Typography
+            variant="h3"
+            style={{
+              textAlign: "center",
             }}
           >
-            <ImageListItem
-              key="Subheader"
-              cols={2}
-              sx={{
-                position: "sticky",
-                top: 0,
-                zIndex: 1,
+            Your ImmuneCrewMates Gallery
+            <Typography
+              style={{
+                color: theme.palette.text.secondary,
+                textAlign: "center",
+                padding: "1rem",
               }}
             >
-              <ListSubheader
-                component="div"
-                sx={{
-                  textAlign: "center",
-                  color: "#fff",
-                  backgroundColor: "#81c784",
-                  fontSize: "1rem",
-                  padding: "0.1rem",
-                }}
-              >
-                ImmuneCrewMates Gallery
-              </ListSubheader>
-            </ImageListItem>
-            {immuneCrewMates.map((crewMate) => (
+              Explore your ImmuneCrewMates and learn more about them. Check our
+              your ImmuneCrewMates' stats and see how they compare to other
+              ImmuneCrewMates.
+            </Typography>
+          </Typography>
+          <Grid item xs={8}>
+            <ImageList
+              sx={{
+                width: "90vw",
+                height: "80vh",
+              }}
+            >
               <ImageListItem
-                key={crewMate.id}
+                key="Subheader"
+                cols={2}
                 sx={{
-                  textAlign: "left",
-                  color: "#fff",
-                  backgroundColor: "#81c784",
-                  fontSize: "1rem",
-                  padding: "0.1rem",
-                  border: "1px solid #81c784",
-                  boxShadow: "3px 3px 3px 0.5px #6fbf73",
-                  margin: "0.5rem",
+                  position: "sticky",
+                  top: 0,
+                  zIndex: 1,
                 }}
               >
-                <img
-                  src={`/src/assets/images/facts/human-body.webp?w=20&fit=crop&auto=format`}
-                  srcSet={`/src/assets/images/facts/human-body.webp?w=20&fit=crop&auto=format&dpr=2 2x`}
-                  alt={crewMate.Name}
-                  loading="eager"
-                />
-                <ImageListItemBar
-                  title={`Name: ${crewMate.Name}`}
-                  subtitle={
-                    <>
-                      <ListItemText
-                        primary={`Cell Type: ${crewMate["Cell Type"]}`}
-                      />
-                      <ListItemText
-                        primary={`Effector Functions: ${crewMate["Effector Functions"]}`}
-                      />
-                      <ListItemText
-                        primary={`Antigen Specificity: ${crewMate["Antigen Specificity"]}`}
-                      />
-                      <ListItemText
-                        primary={`Half-life: ${crewMate["Half-life"]}`}
-                      />
-                      <ListItemText
-                        primary={`Affinity: ${crewMate["Affinity"]}`}
-                      />
-                    </>
-                  }
-                  actionIcon={
-                    <Link href={`/gallery/${crewMate.id}`}>
-                      <IconButton
-                        sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                        aria-label={`info about ${crewMate.Name}`}
-                      >
-                        <EditIcon
-                          sx={{
-                            borderColor: "#81c784",
-                            width: "2rem",
-                            height: "2rem",
-                            padding: "0.5rem",
-                            borderRadius: "50%",
-                            backgroundColor: "#fff",
-                            textTransform: "none",
-                            color: "#28a745",
-                            "&:hover": {
-                              backgroundColor: "#28a745",
-                              color: "#fff",
-                            },
-                          }}
-                        />
-                      </IconButton>
-                    </Link>
-                  }
-                ></ImageListItemBar>
+                <ListSubheader
+                  component="div"
+                  sx={{
+                    textAlign: "center",
+                    color: "#fff",
+                    backgroundColor: "#81c784",
+                    fontSize: "1rem",
+                    padding: "0.1rem",
+                  }}
+                >
+                  ImmuneCrewMates Gallery
+                </ListSubheader>
               </ImageListItem>
-            ))}
-          </ImageList>
-        </Grid>
-        <Grid item xs={4}>
-          <AntigenSpecificityChart />
-          <CellTypeChart />
-          <EffectorFunctionsChart />
-        </Grid>
-      </Grid>
-      <Outlet />
+              {immuneCrewMates.map((crewMate) => (
+                <ImageListItem
+                  key={crewMate.id}
+                  sx={{
+                    textAlign: "left",
+                    color: "#fff",
+                    backgroundColor: "#81c784",
+                    fontSize: "1rem",
+                    padding: "0.1rem",
+                    border: "1px solid #81c784",
+                    boxShadow: "3px 3px 3px 0.5px #6fbf73",
+                    margin: "0.5rem",
+                  }}
+                >
+                  <img
+                    src={`/src/assets/images/facts/human-body.webp?w=20&fit=crop&auto=format`}
+                    srcSet={`/src/assets/images/facts/human-body.webp?w=20&fit=crop&auto=format&dpr=2 2x`}
+                    alt={crewMate.Name}
+                    loading="eager"
+                  />
+                  <ImageListItemBar
+                    title={`Name: ${crewMate.Name}`}
+                    subtitle={
+                      <>
+                        <ListItemText
+                          primary={`Cell Type: ${crewMate["Cell Type"]}`}
+                        />
+                        <ListItemText
+                          primary={`Effector Functions: ${crewMate["Effector Functions"]}`}
+                        />
+                        <ListItemText
+                          primary={`Antigen Specificity: ${crewMate["Antigen Specificity"]}`}
+                        />
+                        <ListItemText
+                          primary={`Half-life: ${crewMate["Half-life"]}`}
+                        />
+                        <ListItemText
+                          primary={`Affinity: ${crewMate["Affinity"]}`}
+                        />
+                      </>
+                    }
+                    actionIcon={
+                      <Link href={`/gallery/${crewMate.id}`}>
+                        <IconButton
+                          sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                          aria-label={`info about ${crewMate.Name}`}
+                        >
+                          <EditIcon
+                            sx={{
+                              borderColor: "#81c784",
+                              width: "2rem",
+                              height: "2rem",
+                              padding: "0.5rem",
+                              borderRadius: "50%",
+                              backgroundColor: "#fff",
+                              textTransform: "none",
+                              color: "#28a745",
+                              "&:hover": {
+                                backgroundColor: "#28a745",
+                                color: "#fff",
+                              },
+                            }}
+                          />
+                        </IconButton>
+                      </Link>
+                    }
+                  ></ImageListItemBar>
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Grid>
+          <Grid container spacing={1}>
+            <Grid
+              item
+              xs={4}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                padding: "1rem",
+                margin: "auto",
+              }}
+            >
+              <AntigenSpecificityChart />
+              <CellTypeChart />
+              <EffectorFunctionsChart />
+            </Grid>
+          </Grid>
+          <Outlet />
+        </>
+      ) : (
+        <CircularProgress />
+      )}
     </>
   );
 };
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# ImmuneCrewMate
-ImmuneCrewMate is a web app for assembling a team of antibodies. Built with Supabase and supabase-js.
+# Web Development Project 7 - _ImmuneCrewMate_
+
+Submitted by: **Arnold Bhebhe**
+.
+
+This web app: **ImmuneCrewMate is a web app for assembling your immune dream team. It mimics soldiers of our immune system**
+
+Time spent: **12** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] **A create form allows users to add new cremates**
+- [x] **Users can name the crewmate and set the crewmate's attributes by clicking on one of several values**
+- [x] **The site displays a summary page of all the user's added crewmates**
+- [x] **A previously created crewmate can be updated and deleted from the crewmate list**
+- [x] **Each crewmate has a direct, unique link to an info page about them**
+
+The following **optional** features are implemented:
+
+- [ ] A crewmate can be given a category upon creation which restricts their attributes
+- [x] The site displays summary statistics about a user's crew on their crew page
+- [ ] The site displays a custom "success" metric about a user's crew which changes the look of the crewmate list
+
+The following **additional** features are implemented:
+
+- [x] Confirmation Page - confirms the action done by the user, and provides guidance on the next steps
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://github.com/SirArnoldB/ImmuneCrewMate/blob/main/ImmuneCrewMate.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+<!-- GIF tool you used! -->
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+- Supabase is a powerful open-source alternative to Firebase for building scalable apps with PostgreSQL
+- Material UI provides pre-built React components for creating attractive and consistent UIs
+- Creating an app with these tools involves configuring the database, integrating the UI components, and managing state
+
+## License
+
+    Copyright [2023] [Arnold Bhebhe]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
